812d1c6798ebe8b1bb1f9580026276e0b3d5404b
pavlogrechka <pavlogrechka@gmail.com>
2021-08-22 12:59:40 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let temp = Math.abs(n).toString();
+  result = +temp.split('').reverse().join('');
+  return result;
 }
